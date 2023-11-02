43ca905861d910d7929c79ef45830c89f0989de6
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-11-02 01:09:19 +0100

    removed the main test class

--- src/test/java/com/youcode/youquiz/YouQuizApplicationTests.java
+++ src/test/java/com/youcode/youquiz/YouQuizApplicationTests.java
@@ -1,13 +1,13 @@
-package com.youcode.youquiz;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class YouQuizApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.youcode.youquiz;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class YouQuizApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
